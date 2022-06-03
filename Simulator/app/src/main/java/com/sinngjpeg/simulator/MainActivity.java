df73f8ce45668c70339844eb0f552b0cba447ba2
sinng <ingridsilvatec@outlook.com>
2022-06-03 00:04:45 -0300

    implementa class mainactivity java

---
+++ Simulator/app/src/main/java/com/sinngjpeg/simulator/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sinngjpeg.simulator;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sinngjpeg.simulator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
