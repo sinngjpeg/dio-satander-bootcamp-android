de0a2647157edcdc112117143702ab979f498f18
sinng <ingridsilvatec@outlook.com>
2022-06-06 23:38:39 -0300

    implementa todos os fields da tela de detalhes

--- Simulator/app/src/main/java/com/sinngjpeg/simulator/ui/DetailActivity.kt
+++ Simulator/app/src/main/java/com/sinngjpeg/simulator/ui/DetailActivity.kt
@@ -26,7 +26,22 @@ class DetailActivity : AppCompatActivity() {
     private fun loadMatchFromExtra() {
         intent?.extras?.getParcelable<Match>(Extras.MATCH)?.let {
             Glide.with(this).load(it.place.image).into(binding.ivPlace)
+            Glide.with(this).load(it.homeTeam.image).into(binding.ivHomeTeam)
+            Glide.with(this).load(it.awayTeam.image).into(binding.ivAwayTeam)
             supportActionBar?.title = it.place.name
+            binding.tvHomeTeamName.text = it.homeTeam.name
+            binding.tvAwayTeamName.text = it.awayTeam.name
+            binding.rbHomeTeamStars.rating = it.homeTeam.stars.toFloat()
+            binding.rbAwayTeamStars.rating = it.awayTeam.stars.toFloat()
+            binding.tvDescription.text = it.description
+            when {
+                it.homeTeam.score != null ->
+                    binding.tvHomeTeamScore.text = it.homeTeam.score.toString()
+            }
+            when {
+                it.awayTeam.score != null ->
+                    binding.tvAwayTeamScore.text = it.homeTeam.score.toString()
+            }
         }
     }
 }
