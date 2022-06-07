72923798cbc31be312ae537358148e532a710e4a
sinng <ingridsilvatec@outlook.com>
2022-06-06 21:18:04 -0300

    implementa classes de domain, mapeando API

---
+++ Simulator/app/src/main/java/com/sinngjpeg/simulator/domain/Match.kt
@@ -0,0 +1,8 @@
+package com.sinngjpeg.simulator.domain
+
+data class Match(
+    val description: String,
+    val place: Place,
+    val homeTeam: Team,
+    val awayTeam: Team
+)
---
+++ Simulator/app/src/main/java/com/sinngjpeg/simulator/domain/Team.kt
@@ -0,0 +1,7 @@
+package com.sinngjpeg.simulator.domain
+
+data class Team(
+    val name: String,
+    val stars: Int,
+    val image: String
+)
