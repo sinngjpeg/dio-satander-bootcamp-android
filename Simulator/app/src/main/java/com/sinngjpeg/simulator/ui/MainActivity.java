af6d9eaf497d41dfc3803a00a5378782c96e177c
sinng <ingridsilvatec@outlook.com>
2022-06-06 21:36:09 -0300

    implementa retrofit

--- Simulator/app/src/main/java/com/sinngjpeg/simulator/ui/MainActivity.java
+++ Simulator/app/src/main/java/com/sinngjpeg/simulator/ui/MainActivity.java
@@ -5,20 +5,34 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sinngjpeg.simulator.data.MatchesAPI;
 import com.sinngjpeg.simulator.databinding.ActivityMainBinding;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private MatchesAPI matchesAPI;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+]
+        setHttpClient();
         setupMatchesList();
         setMatchesRefresh();
         setFloatingActionButton();
+    }
+
+    private void setHttpClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://sinngjpeg.github.io/dio-matches-simulator-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        matchesAPI = retrofit.create(MatchesAPI.class);
     }
 
     private void setFloatingActionButton() {
