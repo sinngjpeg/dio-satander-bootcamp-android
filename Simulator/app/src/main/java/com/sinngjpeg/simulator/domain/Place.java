00928be5f7da6fb52c2cb5f0040d70bf493ecf30
sinng <ingridsilvatec@outlook.com>
2022-06-06 21:49:37 -0300

    add serialized name nas classes de domain

--- Simulator/app/src/main/java/com/sinngjpeg/simulator/domain/Place.java
+++ Simulator/app/src/main/java/com/sinngjpeg/simulator/domain/Place.java
@@ -1,7 +1,11 @@
 package com.sinngjpeg.simulator.domain;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Place {
+    @SerializedName("nome")
     private String name;
+    @SerializedName("imagem")
     private String image;
 
     public String getName() {
--- Simulator/app/src/main/java/com/sinngjpeg/simulator/domain/Team.kt
+++ Simulator/app/src/main/java/com/sinngjpeg/simulator/domain/Team.kt
@@ -1,7 +1,12 @@
 package com.sinngjpeg.simulator.domain
 
+import com.google.gson.annotations.SerializedName
+
 data class Team(
+    @SerializedName("nome")
     val name: String,
+    @SerializedName("forca")
     val stars: Int,
+    @SerializedName("imagem")
     val image: String
 )
