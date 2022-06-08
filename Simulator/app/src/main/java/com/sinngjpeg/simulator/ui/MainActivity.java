8e320a7498ca9ba8fd96823cff60500beb227b69
sinng <ingridsilvatec@outlook.com>
2022-06-07 22:10:56 -0300

    modifica o layout da main activity para coodinator layout

--- Simulator/app/src/main/java/com/sinngjpeg/simulator/ui/MainActivity.java
+++ Simulator/app/src/main/java/com/sinngjpeg/simulator/ui/MainActivity.java
@@ -16,6 +16,7 @@ import com.sinngjpeg.simulator.databinding.ActivityMainBinding;
 import com.sinngjpeg.simulator.domain.Match;
 import com.sinngjpeg.simulator.ui.adapter.MatchesAdapter;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -28,7 +29,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private MatchesAPI matchesAPI;
-    private MatchesAdapter matchesAdapter;
+    private MatchesAdapter matchesAdapter =  new MatchesAdapter(Collections.emptyList());
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -74,7 +75,8 @@ public class MainActivity extends AppCompatActivity {
     private void setupMatchesList() {
         binding.srlMatches.setRefreshing(true);
         binding.rvMatches.setHasFixedSize(true);
-        binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));;
+        binding.rvMatches.setAdapter(matchesAdapter);
         findMatchesFromAPI();
     }
 
